aa0f18d549f5df22a5cdab8ef7b3750a2c86ee44
Justifall <84130430+Angst04@users.noreply.github.com>
2024-06-18 01:01:39 +0300

    some fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+// import { TabsApple } from '/src/mui-treasury/tabs-apple/TabsApple';
 
 import './styles/App.css'
 
@@ -14,7 +15,12 @@ import Counter from './components/Counter';
 import StartButton from './components/StartButton';
 
 function App() {
+  const [valueGlobal, setValueGlobal] = React.useState('1');
   const [value, setValue] = React.useState('1');
+
+  const handleChangeGlobal = (event, newValue) => {
+    setValueGlobal(newValue);
+  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -23,22 +29,38 @@ function App() {
   return (
     <div>
       <Counter />
-      <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChange}>
+      <TabContext value={valueGlobal}>
+        <div className='global-tabs'>
+          <TabList onChange={handleChangeGlobal} textColor='secondary' variant="fullWidth">
             <Tab label="Магазин" value="1" />
             <Tab label="Моя ферма" value="2" />
           </TabList>
-        </Box>
+        </div>
 
         <TabPanel value="1">
-          <Market />
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <TabList onChange={handleChange}>
+                <Tab label="Видеокарты" value="1" />
+                <Tab label="Блоки питания" value="2" />
+              </TabList>
+            </Box>
+
+            <TabPanel value="1">
+              <Market />
+            </TabPanel>
+
+            <TabPanel value="2">
+              Блоки питания
+            </TabPanel>
+          </TabContext>
         </TabPanel>
 
         <TabPanel value="2">
           <Farm />
         </TabPanel>
       </TabContext>
+
 
       <StartButton />
     </div>
