3521d2e0d2d929e0736e89e8bc9e130df0c80143
Justifall <84130430+Angst04@users.noreply.github.com>
2024-06-18 19:05:48 +0300

    changing tabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+// import Box from '@mui/material/Box';
+// import Tab from '@mui/material/Tab';
+// import TabContext from '@mui/lab/TabContext';
+// import TabList from '@mui/lab/TabList';
+// import TabPanel from '@mui/lab/TabPanel';
 // import { TabsApple } from '/src/mui-treasury/tabs-apple/TabsApple';
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 import './styles/App.css'
 
@@ -15,51 +18,37 @@ import Counter from './components/Counter';
 import StartButton from './components/StartButton';
 
 function App() {
-  const [valueGlobal, setValueGlobal] = React.useState('1');
-  const [value, setValue] = React.useState('1');
-
-  const handleChangeGlobal = (event, newValue) => {
-    setValueGlobal(newValue);
-  };
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
   return (
     <div>
       <Counter />
-      <TabContext value={valueGlobal}>
-        <div className='global-tabs'>
-          <TabList onChange={handleChangeGlobal} textColor='secondary' variant="fullWidth">
-            <Tab label="Магазин" value="1" />
-            <Tab label="Моя ферма" value="2" />
-          </TabList>
-        </div>
 
-        <TabPanel value="1">
-          <TabContext value={value}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-              <TabList onChange={handleChange}>
-                <Tab label="Видеокарты" value="1" />
-                <Tab label="Блоки питания" value="2" />
-              </TabList>
-            </Box>
+      <Tabs>
+        <TabList className='global-tabs'>
+          <Tab>Магазин</Tab>
+          <Tab>Моя ферма</Tab>
+        </TabList>
 
-            <TabPanel value="1">
+        <TabPanel>
+          <Tabs>
+            <TabList>
+              <Tab>Видеокарты</Tab>
+              <Tab>Блоки питания</Tab>
+            </TabList>
+
+            <TabPanel>
               <Market />
             </TabPanel>
 
-            <TabPanel value="2">
+            <TabPanel>
               Блоки питания
             </TabPanel>
-          </TabContext>
+          </Tabs>
         </TabPanel>
 
-        <TabPanel value="2">
+        <TabPanel>
           <Farm />
         </TabPanel>
-      </TabContext>
+      </Tabs>
 
 
       <StartButton />
